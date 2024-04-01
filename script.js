86a8f64aba5c54bb9c3ad7c56f1322f0f4392f02
Osman Yakub Beni <osmanmalone779@gmail.com>
2024-04-01 14:53:52 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+let apiQuotes = [];
+
+// Show new Quote
+
+function newQuote() {
+  // Pick a random quotes from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  // Check if author field is blank and replace it with unknown
+  if (quote.author.split(",").length != 2) {
+    authorText.textContent = "unknown";
+  } else {
+    authorText.textContent = quote.author.split(",")[0];
+  }
+
+  // Check quote length to determine the stying
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+}
+
+// Get Quotes From API
+
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //Catch Error Here
+  }
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// Onload
+getQuotes();
