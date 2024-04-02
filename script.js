ce0fe6144ab8d7e78f1316a22b84c341d3ac696b
Osman Yakub Beni <osmanmalone779@gmail.com>
2024-04-02 14:57:11 +0100

    add loader

--- script.js
+++ script.js
@@ -3,29 +3,48 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 // let apiQuotes = [];
+
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
 // Show new Quote
 
 function newQuote() {
+  loading();
   // Pick a random quotes from apiQuotes array
   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
 
   // Check if author field is blank and replace it with unknown
-  if (!quote.author) {
-    authorText.textContent = "unknown";
-  } else {
-    authorText.textContent = quote.author;
-  }
+  setTimeout(() => {
+    if (!quote.author) {
+      authorText.textContent = "unknown";
+    } else {
+      authorText.textContent = quote.author;
+    }
 
-  // Check quote length to determine the stying
-  if (quote.text.length > 120) {
-    quoteText.classList.add("long-quote");
-  } else {
-    quoteText.classList.remove("long-quote");
-  }
-  quoteText.textContent = quote.text;
+    // Check quote length to determine the stying
+    if (quote.text.length > 120) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+
+    // Set quote, hide loader
+    quoteText.textContent = quote.text;
+    complete();
+  }, 1000);
 }
 
 // Get Quotes From API
